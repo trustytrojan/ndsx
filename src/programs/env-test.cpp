1401c8082591d87bf688f8300b72dd047dbbb759
trustytrojan <87675609+trustytrojan@users.noreply.github.com>
2026-03-17 20:49:34 -0400

    increase env test coverage

--- src/programs/env-test.cpp
+++ src/programs/env-test.cpp
@@ -1,7 +1,34 @@
 #include <cstdio>
 #include <cstdlib>
+#include <string_view>
 
-int main()
+using namespace std::literals;
+
+int main(int, char **, char **envp)
 {
-	printf("env-test: X=%s Y=%s Z=%s\n", getenv("X"), getenv("Y"), getenv("Z"));
+	puts("env-test:");
+
+	puts(" initial envp:");
+	for (auto s{envp}; *s; ++s)
+		printf("  '%s'\n", *s);
+
+	// printf(" getenv test:\n  X='%s' Y='%s' Z='%s'\n", getenv("X"), getenv("Y"), getenv("Z"));
+
+	// puts(" setenv test: Y <- 67");
+	setenv("Y", "67", 1);
+	// const auto yval = getenv("Y");
+	// printf("  Y='%s'\n", yval);
+	// if (yval != "67"sv)
+	// 	puts("  FAILED");
+
+	// puts(" unsetenv test: Z");
+	unsetenv("Z");
+	// const auto zval = getenv("Z");
+	// printf("  Z='%s'\n", zval);
+	// if (zval)
+	// 	puts("  FAILED");
+
+	puts(" final envp:");
+	for (auto s{envp}; *s; ++s)
+		printf("  '%s'\n", *s);
 }
